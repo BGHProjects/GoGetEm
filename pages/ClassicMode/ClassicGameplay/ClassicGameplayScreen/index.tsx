26393fffca0cf8c0e16e7b354281edb8c046418a
BGHProjects <un12known34_acc@outlook.com>
2021-09-11 18:52:04 +1000

    Restricted player movement, and implemented A* to find player

--- pages/ClassicMode/ClassicGameplay/ClassicGameplayScreen/index.tsx
+++ pages/ClassicMode/ClassicGameplay/ClassicGameplayScreen/index.tsx
@@ -18,7 +18,8 @@ const cellSize = height * 0.04;
 const mazeGrid: any = [];
 const wallWidth = 1;
 const stack: any = [];
-let gridColor = "blue";
+let gridColor = "violet";
+let searchGrid1: any = [];
 
 const generateCells = () => {
   mazeGrid.length = 0;
@@ -45,16 +46,16 @@ const checkNeighbours = (cell: any) => {
 
   // Finds the neighbours in the grid
   let top = mazeGrid.filter(
-    (item) => item.row === cell.row - 10 && item.col === cell.col
+    (item: any) => item.row === cell.row - 10 && item.col === cell.col
   );
   let right = mazeGrid.filter(
-    (item) => item.col === cell.col + 10 && item.row === cell.row
+    (item: any) => item.col === cell.col + 10 && item.row === cell.row
   );
   let bottom = mazeGrid.filter(
-    (item) => item.row === cell.row + 10 && item.col === cell.col
+    (item: any) => item.row === cell.row + 10 && item.col === cell.col
   );
   let left = mazeGrid.filter(
-    (item) => item.col === cell.col - 10 && item.row === cell.row
+    (item: any) => item.col === cell.col - 10 && item.row === cell.row
   );
 
   // If there is a neighbour for that cell, add it to defined
@@ -111,7 +112,7 @@ const makeMaze = () => {
   current.visited = true;
 
   for (let i = 0; i < 300; i++) {
-    if (mazeGrid.every((cell) => cell.visited === true) === false) {
+    if (mazeGrid.every((cell: any) => cell.visited === true) === false) {
       // Get the next cell to visit
       let next = checkNeighbours(current);
       if (next) {
@@ -127,40 +128,197 @@ const makeMaze = () => {
   }
 };
 
+function makeSearchGrid() {
+  searchGrid1 = [...mazeGrid];
+
+  for (let i = 0; i < searchGrid1.length; i++) {
+    searchGrid1[i].f = 0;
+    searchGrid1[i].g = 0;
+    searchGrid1[i].h = 0;
+    searchGrid1[i].neighbours = [];
+    searchGrid1[i].previous = null;
+  }
+
+  return searchGrid1;
+}
+
+function findNeighbour(cell: any) {
+  let defined = [];
+
+  let top = searchGrid1.filter(
+    (item: any) => item.row === cell.row - 10 && item.col === cell.col
+  );
+  let right = searchGrid1.filter(
+    (item: any) => item.col === cell.col + 10 && item.row === cell.row
+  );
+  let bottom = searchGrid1.filter(
+    (item: any) => item.row === cell.row + 10 && item.col === cell.col
+  );
+  let left = searchGrid1.filter(
+    (item: any) => item.col === cell.col - 10 && item.row === cell.row
+  );
+
+  // If there is a neighbour for that cell, add it to defined
+  if (top.length > 0) {
+    if (top[0].bottom === 0) {
+      defined.push(top[0]);
+    }
+  }
+
+  if (right.length > 0) {
+    if (right[0].left === 0) {
+      defined.push(right[0]);
+    }
+  }
+
+  if (bottom.length > 0) {
+    if (bottom[0].top === 0) {
+      defined.push(bottom[0]);
+    }
+  }
+
+  if (left.length > 0) {
+    if (left[0].right === 0) {
+      defined.push(left[0]);
+    }
+  }
+
+  // Add all unvisited neighbours to neighbours
+  for (let i = 0; i < defined.length; i++) {
+    cell.neighbours.push(defined[i]);
+  }
+}
+
 generateCells();
 makeMaze();
+makeSearchGrid();
 
 const ClassicGameplayScreen = ({ navigation }) => {
-  const [topPressed, setTopPressed] = useState(false);
-  const [rightPressed, setRightPressed] = useState(false);
-  const [bottomPressed, setBottomPressed] = useState(false);
-  const [leftPressed, setLeftPressed] = useState(false);
   const [playerX, setplayerX] = useState(5);
   const [playerY, setplayerY] = useState(5);
+  const [player2X, setplayer2X] = useState(95);
+  const [player2Y, setplayer2Y] = useState(95);
 
   const movePlayerUp = () => {
-    if (playerY > 5) {
+    let mazeCell = getPlayerMazeCell();
+
+    if (playerY > 5 && mazeCell.top === 0) {
       setplayerY(playerY - 10);
     }
   };
 
   const movePlayerRight = () => {
-    if (playerX > 5) {
-      setplayerX(playerX - 10);
-    }
-  };
+    let mazeCell = getPlayerMazeCell();
 
-  const movePlayerLeft = () => {
-    if (playerX < 95) {
+    if (playerX < 95 && mazeCell.right === 0) {
       setplayerX(playerX + 10);
     }
   };
 
+  const movePlayerLeft = () => {
+    let mazeCell = getPlayerMazeCell();
+
+    if (playerX > 5 && mazeCell.left === 0) {
+      setplayerX(playerX - 10);
+    }
+  };
+
   const movePlayerDown = () => {
-    if (playerY < 95) {
+    let mazeCell = getPlayerMazeCell();
+
+    if (playerY < 95 && mazeCell.bottom === 0) {
       setplayerY(playerY + 10);
     }
   };
+
+  const getPlayerMazeCell = () => {
+    let digit1 = (playerX - 5).toString();
+    let digit2 = (playerY - 5).toString();
+    let digits;
+
+    if (digit2 === "0") {
+      digits = digit1[0];
+    } else {
+      digits = digit2[0] + digit1[0];
+    }
+
+    return mazeGrid[digits];
+  };
+
+  function heuristic(a: any, b: any) {
+    let xs = Number(b.row) - Number(a.row);
+    let ys = Number(b.col) - Number(a.col);
+    let answer = Math.abs(xs) + Math.abs(ys);
+
+    return answer;
+  }
+
+  const aStarSearch = (playerPosition: any) => {
+    let openSet = [];
+    let closedSet = [];
+    let start = searchGrid1[99];
+    let end = playerPosition;
+
+    openSet.push(start);
+
+    while (openSet.length > 0) {
+      let lowestIndex = 0;
+
+      let current: any = openSet[lowestIndex];
+
+      if (current === end) {
+        let path = [];
+        let temp = current;
+        path.push(temp);
+        while (temp.previous) {
+          path.push(temp.previous);
+          temp = temp.previous;
+        }
+
+        for (let i = 0; i < path.length; i++) {
+          path[i].color = "blue";
+        }
+
+        //console.log("\n Object.keys(path[0]) ", Object.keys(path[0]));
+        //console.log("\n path[0].color ", path[0].color);
+
+        console.log("Done");
+      }
+
+      openSet.splice(lowestIndex, 1);
+      // openSet.remove(current)
+      closedSet.push(current);
+
+      findNeighbour(current);
+      for (let i = 0; i < current.neighbours.length; i++) {
+        let neighbour: any = current.neighbours[i];
+        if (!closedSet.includes(neighbour)) {
+          let tempG = current.g + 1;
+
+          if (openSet.includes(neighbour)) {
+            if (tempG < neighbour.g) {
+              neighbour.g = tempG;
+            }
+          } else {
+            neighbour.g = tempG;
+            openSet.push(neighbour);
+          }
+
+          neighbour.h = heuristic(neighbour, end);
+          neighbour.f = neighbour.g + neighbour.h;
+          neighbour.previous = current;
+        }
+      }
+    }
+  };
+
+  useEffect(() => {
+    for (let i = 0; i < searchGrid1.length; i++) {
+      searchGrid1[i].color = null;
+    }
+    let cell = getPlayerMazeCell();
+    aStarSearch(cell);
+  }, [playerX, playerY]);
 
   return (
     <SafeAreaView style={styles.container}>
@@ -184,7 +342,9 @@ const ClassicGameplayScreen = ({ navigation }) => {
             }}
           />
         ))}
+
         <Svg height="100%" width="100%" viewBox="0 0 100 100">
+          <Circle cx={player2X} cy={player2Y} r="3" fill="lightgreen"></Circle>
           <Circle cx={playerX} cy={playerY} r="3" fill="red"></Circle>
         </Svg>
       </View>
@@ -211,7 +371,7 @@ const ClassicGameplayScreen = ({ navigation }) => {
             cy="50%"
             r={height / 52}
             fill="orange"
-            onPress={() => movePlayerRight()}
+            onPress={() => movePlayerLeft()}
           />
           <Circle
             cx="45%"
@@ -225,7 +385,7 @@ const ClassicGameplayScreen = ({ navigation }) => {
             cy="50%"
             r={height / 52}
             fill="orange"
-            onPress={() => movePlayerLeft()}
+            onPress={() => movePlayerRight()}
           />
         </Svg>
       </View>
